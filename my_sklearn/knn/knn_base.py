e8edd3d8fa971e1194a0faac67f517b18f8737ea
Fernando Contreras <fertueros@gmail.com>
2023-08-26 01:32:32 -0500

    knn base

---
+++ my_sklearn/knn/knn_base.py
@@ -0,0 +1,19 @@
+from distance_metrics import*
+class KNNBase:
+    def __init__(self, k=5, metric='euclidean'):
+        self.k = k
+        self.metric = metric
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+    
+    def _compute_distance(self, x1, x2):
+        if self.metric == 'euclidean':
+            return euclidean_distance(x1, x2)
+        elif self.metric == 'manhattan':
+            return manhattan_distance(x1, x2)
+        else:
+            raise ValueError(f"Unknown metric: {self.metric}")
+
+    
